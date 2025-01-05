442e760c6195ec0e01607aa1b544d63d0e845a38
gaurav23617 <gaurav23617@gmail.com>
2025-01-05 18:49:28 +0530

    .

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,6 +2,7 @@
 alias c='clear'
 alias vim='nvim'
 alias g='lazygit'
+alias ff='fastfetch'
 alias nvimrc='nvim ~/.config/nvim/'
 alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
 alias sshk="kitty +kitten ssh"
