35857c3a9f05d8f9705dceae5b6f8e341b99b705
gaurav23617 <gaurav23617@gmail.com>
2025-04-30 05:23:27 +0100

    rebuild path changed to dotfile from NixOs

--- modules/desktop/hyprland/scripts/rebuild.sh
+++ modules/desktop/hyprland/scripts/rebuild.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# Colors for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 if [[ $EUID -eq 0 ]]; then
   echo "This script should not be executed as root! Exiting..."
@@ -16,39 +15,33 @@ if [[ ! "$(grep -i nixos </etc/os-release)" ]]; then
   exit 1
 fi
 
-if [ -f "$HOME/NixOS/flake.nix" ]; then
-  flake=$HOME/NixOS
+if [ -f "$HOME/dotfile/flake.nix" ]; then
+  flake=$HOME/dotfile
 elif [ -f "/etc/nixos/flake.nix" ]; then
   flake=/etc/nixos
 else
-  echo "Error: flake not found. ensure flake.nix exists in either $HOME/NixOS or /etc/nixos"
+  echo "Error: flake not found. Ensure flake.nix exists in either \$HOME/dotfile or /etc/nixos"
   exit 1
 fi
+
 echo -e "${GREEN}Rebuilding from $flake${NC}"
 currentUser=$(logname)
 
-# replace username variable in flake.nix with $USER
 sudo sed -i -e "s/username = \".*\"/username = \"$currentUser\"/" "$flake/flake.nix"
 
+hardware_cfg_path="$flake/hosts/Default/hardware-configuration.nix"
+
 if [ -f "/etc/nixos/hardware-configuration.nix" ]; then
-  cat "/etc/nixos/hardware-configuration.nix" | sudo tee "$flake/hosts/Default/hardware-configuration.nix" >/dev/null
+  sudo cp /etc/nixos/hardware-configuration.nix "$hardware_cfg_path"
 elif [ -f "/etc/nixos/hosts/Default/hardware-configuration.nix" ]; then
-  cat "/etc/nixos/hosts/Default/hardware-configuration.nix" | sudo tee "$flake/hosts/Default/hardware-configuration.nix" >/dev/null
+  sudo cp /etc/nixos/hosts/Default/hardware-configuration.nix "$hardware_cfg_path"
 else
-  # read -p "No hardware config found, generate another? (Y/n): " confirm
-  # if [[ "$confirm" =~ ^[nN]$ ]]; then
-  #   echo "Aborted."
-  #   exit 1
-  # fi
-  sudo nixos-generate-config --show-hardware-config >"$flake/hosts/Default/hardware-configuration.nix"
+  sudo nixos-generate-config --show-hardware-config >"$hardware_cfg_path"
 fi
 
-sudo git -C "$flake" add hosts/Default/hardware-configuration.nix
-
-# nh os switch "$flake"
+sudo git -C "$flake" add "hosts/Default/hardware-configuration.nix"
 sudo nixos-rebuild switch --flake "$flake#Default"
-# rm "$flake"/hosts/Default/hardware-configuration.nix &>/dev/null
-# git restore --staged "$flake"/hosts/Default/hardware-configuration.nix &>/dev/null
 
 echo
 read -rsn1 -p"$(echo -e "${GREEN}Press any key to continue${NC}")"
+
