908700e69b1c1f128781aa28fb3e468e675a9eb2
gaurav23617 <gaurav23617@gmail.com>
2025-05-06 21:26:36 +0530

    trying to add ghostty config

--- modules/programs/terminal/ghostty/default.nix
+++ modules/programs/terminal/ghostty/default.nix
@@ -8,31 +8,23 @@
 
 with lib;
 
-let
-  cfg = config.within.ghostty;
-  config_name = if pkgs.stdenv.isLinux then "linux-config" else "mac-config";
-in
 {
   options.within.ghostty.enable = mkEnableOption "Enables ghostty Terminal Settings";
 
-  config = mkIf cfg.enable {
-
+  config = mkIf config.within.ghostty.enable {
     nixpkgs = {
       overlays = [
         inputs.brew-nix.overlays.default
       ];
     };
-    home.packages = (
-      if pkgs.stdenv.isLinux then
-        [
-          inputs.ghostty.packages."${pkgs.system}".default
-        ]
-      else
-        [ pkgs.brewCasks.ghostty ]
-    );
+
+    home.packages = [
+      inputs.ghostty.packages."${pkgs.system}".default
+    ];
+
     home.file = {
-      ".config/ghostty/${config_name}" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/ghostty/config";
+      ".config/ghostty/config" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfile/ghostty/config";
       };
     };
   };
