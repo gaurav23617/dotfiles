35d6c1373c3719a476bf5687d25973b954391dbd
gaurav23617 <gaurav23617@gmail.com>
2025-05-07 09:54:01 +0530

    change zsh/defailt to shell/default

---
+++ modules/default.nix
@@ -0,0 +1,37 @@
+{
+  videoDriver,
+  browser,
+  editor,
+  terminal,
+  terminalFileManager,
+  ...
+}:
+
+{
+  imports = [
+    ./hardware/video/${videoDriver}.nix # Enable gpu drivers defined in flake.nix
+    ./hardware/drives
+
+    ./scripts
+
+    ./desktop/hyprland # Enable hyprland window manager
+
+    ./programs/games
+    ./programs/browser/${browser} # Set browser defined in flake.nix
+    ./programs/terminal/${terminal} # Set terminal defined in flake.nix
+    ./programs/editor/${editor} # Set editor defined in flake.nix
+    ./programs/cli/${terminalFileManager} # Set file-manager defined in flake.nix
+    ./programs/cli/starship
+    ./programs/cli/tmux
+    ./programs/cli/direnv
+    ./programs/cli/lazygit
+    ./programs/cli/cava
+    ./programs/cli/btop
+    ./programs/shell
+    ./programs/media/discord
+    ./programs/media/spicetify
+    ./programs/misc/tlp
+    ./programs/misc/virt-manager
+    ./programs/misc/lact # gpu power and fan control (WIP)
+  ];
+}
