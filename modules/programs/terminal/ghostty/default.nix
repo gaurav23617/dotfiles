358e051dfa5bb206fee92e90b6b5400cbe8f1476
gaurav23617 <gaurav23617@gmail.com>
2025-05-07 09:53:14 +0530

    removed extra terminal

--- modules/programs/terminal/ghostty/default.nix
+++ modules/programs/terminal/ghostty/default.nix
@@ -8,6 +8,11 @@
 
 with lib;
 
+let
+  # Detect if the system is Linux or macOS
+  isLinux = pkgs.stdenv.isLinux;
+  config_name = if isLinux then "linux-config" else "mac-config";
+in
 {
   options.within.ghostty.enable = mkEnableOption "Enables ghostty Terminal Settings";
 
@@ -18,13 +23,23 @@ with lib;
       ];
     };
 
-    home.packages = [
-      inputs.ghostty.packages."${pkgs.system}".default
-    ];
+    # Conditional package inclusion based on the system (Linux vs Mac)
+    home.packages = (
+      if isLinux then
+        [
+          inputs.ghostty.packages."${pkgs.system}".default
+        ]
+      else
+        [ pkgs.brewCasks.ghostty ] # Use Homebrew for macOS
+    );
 
+    # Files for both Linux and macOS, symlinks to dotfiles folder
     home.file = {
       ".config/ghostty/config" = {
         source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfile/ghostty/config";
+      };
+      ".config/ghostty/${config_name}" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfile/ghostty/${config_name}";
       };
     };
   };
