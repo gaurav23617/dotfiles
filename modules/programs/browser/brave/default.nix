0cc79847e9ac5dcedc17ae0c2997353d87f81a29
gaurav23617 <gaurav23617@gmail.com>
2025-05-09 06:49:39 +0530

    brave browser added

--- modules/programs/browser/brave/default.nix
+++ modules/programs/browser/brave/default.nix
@@ -1,18 +1,39 @@
-{ pkgs, ... }:
-
 {
-  home.packages = [
-    pkgs.brave
-    pkgs.google-chrome
-  ];
+  home-manager.sharedModules = [
+    (
+      { pkgs, ... }:
+      {
+        home.packages = [
+          pkgs.brave
+        ];
 
-  programs.chromium = {
-    enable = true;
-    extensions = [
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; }
-    ];
-    commandLineArgs = [
-      "--disable-features=WebRtcAllowInputVolumeAdjustment"
-    ];
-  };
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "text/html" = "brave-browser.desktop";
+            "x-scheme-handler/http" = "brave-browser.desktop";
+            "x-scheme-handler/https" = "brave-browser.desktop";
+          };
+        };
+        # Brave managed policies: force-install extensions, set homepage
+        environment.etc."opt/brave/policies/managed/managed_policies.json".text = builtins.toJSON {
+          ExtensionInstallForcelist = [
+            "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+            "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+          ];
+          RestoreOnStartup = 4;
+          HomepageLocation = "https://startpage.com";
+          DefaultBrowserSettingEnabled = true;
+        };
+
+        # Optional: Add a desktop shortcut
+        programs.bash = {
+          enable = true;
+          shellAliases = {
+            brave = "brave-browser";
+          };
+        };
+      }
+    )
+  ];
 }
