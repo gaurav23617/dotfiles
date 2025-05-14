a83e3f57cc075cf68418299d713971fb17432d0f
gaurav23617 <gaurav23617@gmail.com>
2025-05-14 02:19:05 +0000

    code clearned

--- modules/programs/shell/zsh/default.nix
+++ modules/programs/shell/zsh/default.nix
@@ -11,14 +11,10 @@
         programs.zsh = {
           enable = true;
           dotDir = ".config/zsh";
-          # Other Zsh options like history, aliases, initContent, etc...
         };
 
-        # Symlink your Zsh folder (e.g. contains .zshrc, .p10k.zsh, etc.)
         home.file.".config/zsh/.zshrc".source =
           config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/zsh/.zshrc";
-
-        # DO NOT SYMLINK `.zshenv` ANYWHERE — let Home Manager manage /etc/zshenv
       }
     )
   ];
