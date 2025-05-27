264cb75847b91863f12c5b1c0dd180a7c1214d97
gaurav23617 <gaurav23617@gmail.com>
2025-05-27 21:16:21 +0530

    trying to fix it

--- modules/programs/media/spicetify/default.nix
+++ modules/programs/media/spicetify/default.nix
@@ -1,34 +1,44 @@
 { inputs, lib, ... }:
+
 {
+  # Allow unfree packages like Spotify
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "spotify" ];
 
   home-manager.sharedModules = [
     (
       { pkgs, ... }:
+
       let
         spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
       in
+
       {
-        # Only install actual programs here
+        # Install only actual binaries
         home.packages = with pkgs; [
           spotify
           spicetify-cli
         ];
 
-        # Import Spicetify module
-        imports = [ inputs.spicetify-nix.homeManagerModules.default ];
+        # Import spicetify-nix module
+        imports = [
+          inputs.spicetify-nix.homeManagerModules.default
+        ];
 
-        # Use theme only inside spicetify config (no global install)
+        # Configure spicetify, safely referencing theme
         programs.spicetify = {
           enable = true;
+
+          # Reference theme only inside Spicetify config (do not install globally)
           theme = spicePkgs.themes.catppuccin;
           colorScheme = "mocha";
+
           enabledExtensions = with spicePkgs.extensions; [
             shuffle
             keyboardShortcut
             copyLyrics
             fullAppDisplay
           ];
+
           enabledCustomApps = with spicePkgs.apps; [
             marketplace
             localFiles
