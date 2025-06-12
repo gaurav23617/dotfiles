73d8bc9a02bb9f56a13547ad97ca392c039e9937
gaurav23617 <gaurav23617@gmail.com>
2025-06-12 22:51:41 +0530

    user created

---
+++ users/gaurav.nix
@@ -0,0 +1,32 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  # TODO: Set your username
+  home = {
+    username = "gaurav";
+    homeDirectory = "/home/gaurav";
+  };
+
+  # You can import other home-manager modules here
+  imports = [
+  ];
+
+  # Add stuff for your user as you see fit:
+  programs.neovim.enable = true;
+  # home.packages = with pkgs; [ steam ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
