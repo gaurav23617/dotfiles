6e8534c1c36eec9101617551af05613b6cabe25c
gaurav23617 <gaurav23617@gmail.com>
2025-06-12 22:51:26 +0530

    host added

---
+++ host/coffee/home.nix
@@ -0,0 +1,38 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  # You can import other home-manager modules here
+  imports = [
+    ../../users/gaurav.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  # Host-specific packages and config
+  home.packages = with pkgs; [
+    # Packages specific to this machine
+  ];
+
+  systemd.user.startServices = "sd-switch";
+}
