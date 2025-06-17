b836332810c88e5b11c02619b326dadba348b886
gaurav23617 <gaurav23617@gmail.com>
2025-06-18 04:29:36 +0530

    services added

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,13 +1,10 @@
-# Add your reusable NixOS modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
-# These should be stuff you would like to share with others, not your personal configurations.
 {
-  # List your module files here
-  # my-module = import ./my-module.nix;
   imports = [
     ./networking.nix
     ./bluetooth.nix
     ./security.nix
     ./sound.nix
     ./xserver.nix
+    ./services.nix
   ];
 }
---
+++ modules/nixos/services.nix
@@ -0,0 +1,32 @@
+{
+  # System services
+  services = {
+    btrfs.autoScrub = {
+      enable = true;
+      interval = "weekly";
+      fileSystems = [ "/" ];
+    };
+
+    fstrim.enable = true; # SSD life
+
+    # Thunar
+    gvfs.enable = true; # Mount, trash, and other functionalities
+    tumbler.enable = true; # Thumbnail support for images
+
+    dbus.enable = true;
+    udisks2.enable = true;
+
+    # SSH
+    # openssh = {
+    #   enable = true;
+    #   ports = [22];
+    #   settings = {
+    #     PasswordAuthentication = true;
+    #     AllowUsers = null;
+    #     UseDns = true;
+    #     X11Forwarding = false;
+    #     PermitRootLogin = "prohibit-password";
+    #   };
+    # };
+  };
+}
