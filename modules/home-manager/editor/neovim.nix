aaa03f206c4b49f2e874b598280e594e4974e7c4
gaurav23617 <gaurav23617@gmail.com>
2025-06-18 19:26:26 +0530

    neovim config added and enable option added

--- modules/home-manager/editor/neovim.nix
+++ modules/home-manager/editor/neovim.nix
@@ -1,105 +1,90 @@
-{ ... }:
 {
-  home-manager.sharedModules = [
-    (
-      {
-        config,
-        pkgs,
-        lib,
-        ...
-      }:
-      let
-        inherit (pkgs)
-          tree-sitter
-          lua54Packages
-          luajitPackages
-          nodePackages_latest
-          vimPlugins
-          ;
-        inherit (pkgs.tree-sitter-grammars)
-          tree-sitter-lua
-          tree-sitter-nix
-          tree-sitter-go
-          tree-sitter-python
-          tree-sitter-bash
-          tree-sitter-regex
-          tree-sitter-markdown
-          tree-sitter-json
-          ;
-      in
-      {
-        programs.neovim = {
-          enable = true;
-          vimAlias = true;
-          vimdiffAlias = true;
-          extraPackages = with pkgs; [
-            # Tree-sitter and grammars
-            tree-sitter
-            tree-sitter-lua
-            tree-sitter-nix
-            tree-sitter-go
-            tree-sitter-python
-            tree-sitter-bash
-            tree-sitter-regex
-            tree-sitter-markdown
-            tree-sitter-json
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
-            # LSP servers (Nix-managed, not Mason)
-            lua-language-server
-            nixd
-            nil
-            gopls
-            rust-analyzer
-            basedpyright
-            nodePackages_latest.vscode-json-languageserver
-            nodePackages_latest.vscode-langservers-extracted # HTML, CSS, JSON
-            nodePackages_latest.typescript-language-server
-            nodePackages_latest.bash-language-server
-            nodePackages_latest.dockerfile-language-server-nodejs
-            nodePackages_latest.yaml-language-server
-            marksman # Markdown LSP
+with lib;
 
-            # Formatters and linters
-            stylua
-            nixfmt-rfc-style
-            gofumpt
-            prettier
-            black
-            shfmt
+let
+  cfg = config.features.cli.neovim;
+in
+{
+  options.features.cli.neovim.enable = mkEnableOption "Enable Neovim";
 
-            # Development tools
-            gcc
-            gnumake
-            go
-            rustc
-            cargo
-            nodejs_20
-            python3
+  config = mkIf cfg.enable {
+    programs.neovim = {
+      enable = true;
+      vimAlias = true;
+      vimdiffAlias = true;
 
-            # Utilities
-            fzf
-            ripgrep
-            fd
-            unzip
-            git
-            curl
-            steam-run
+      extraPackages = with pkgs; [
+        # Tree-sitter and grammars
+        tree-sitter
+        tree-sitter-lua
+        tree-sitter-nix
+        tree-sitter-go
+        tree-sitter-python
+        tree-sitter-bash
+        tree-sitter-regex
+        tree-sitter-markdown
+        tree-sitter-json
 
-            # Lua packages
-            lua54Packages.jsregexp
-            luajitPackages.jsregexp
-          ];
+        # LSP servers (Nix-managed, not Mason)
+        lua-language-server
+        nixd
+        nil
+        gopls
+        rust-analyzer
+        basedpyright
+        nodePackages_latest.vscode-json-languageserver
+        nodePackages_latest.vscode-langservers-extracted # HTML, CSS, JSON
+        nodePackages_latest.typescript-language-server
+        nodePackages_latest.bash-language-server
+        nodePackages_latest.dockerfile-language-server-nodejs
+        nodePackages_latest.yaml-language-server
+        marksman # Markdown LSP
 
-          plugins = with vimPlugins; [
-            nvim-treesitter.withAllGrammars
-          ];
-        };
+        # Formatters and linters
+        stylua
+        nixfmt-rfc-style
+        gofumpt
+        prettier
+        black
+        shfmt
 
-        # Your LazyVim config
-        home.file.".config/nvim".source = builtins.toString (
-          config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/nvim"
-        );
-      }
-    )
-  ];
+        # Development tools
+        gcc
+        gnumake
+        go
+        rustc
+        cargo
+        nodejs_20
+        python3
+
+        # Utilities
+        fzf
+        ripgrep
+        fd
+        unzip
+        git
+        curl
+        steam-run
+
+        # Lua packages
+        lua54Packages.jsregexp
+        luajitPackages.jsregexp
+      ];
+
+      plugins = with vimPlugins; [
+        nvim-treesitter.withAllGrammars
+      ];
+    };
+
+    # Your LazyVim config
+    home.file.".config/nvim".source = builtins.toString (
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/nvim"
+    );
+  };
 }
