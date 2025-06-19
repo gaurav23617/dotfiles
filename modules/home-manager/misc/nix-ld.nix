ad4130f668ed69eed9d4fc57e1b66d4aeb1ac7fc
gaurav23617 <gaurav23617@gmail.com>
2025-06-19 22:03:56 +0530

    nix-ld added

---
+++ modules/home-manager/misc/nix-ld.nix
@@ -0,0 +1,74 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.programs.nix-ld;
+in
+{
+  options.programs.nix-ld.enable = mkEnableOption "Enable nix-ld for dynamic linking with Nix packages";
+  config = mkIf cfg.enable {
+    programs.nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        alsa-lib
+        at-spi2-atk
+        at-spi2-core
+        atk
+        cairo
+        cups
+        curl
+        dbus
+        expat
+        fontconfig
+        freetype
+        fuse
+        fuse3
+        gdk-pixbuf
+        glib
+        gtk3
+        harfbuzz
+        icu
+        libGL
+        libappindicator-gtk3
+        libdrm
+        libglvnd
+        libnotify
+        libpulseaudio
+        libunwind
+        libusb1
+        libuuid
+        libxkbcommon
+        libxml2
+        mesa
+        nspr
+        nss
+        openssl
+        pango
+        pipewire
+        stdenv.cc.cc
+        systemd
+        vulkan-loader
+        xorg.libX11
+        xorg.libXScrnSaver
+        xorg.libXcomposite
+        xorg.libXcursor
+        xorg.libXdamage
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXi
+        xorg.libXrandr
+        xorg.libXrender
+        xorg.libXtst
+        xorg.libxcb
+        xorg.libxkbfile
+        xorg.libxshmfence
+        zlib
+      ];
+    };
+  };
+
+}
