b33939cae0c67ef2eaa5487fde6d33c9169b678a
gaurav23617 <gaurav23617@gmail.com>
2025-06-19 22:02:01 +0530

    lact with config added

---
+++ modules/home-manager/misc/lact.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.home-manager.misc.lact;
+in
+{
+  options.home-manager.misc.lact.enable =
+    mkEnableOption "Enable lact (Linux Advanced Configuration Tool)";
+
+  config = mkIf cfg.enable {
+    systemd = {
+      packages = with pkgs; [ lact ];
+      services.lactd.wantedBy = [ "multi-user.target" ];
+    };
+    environment.systemPackages = with pkgs; [ lact ];
+  };
+}
