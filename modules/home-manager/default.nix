818497d35d3c0f2a345657c58b52b66007a3d824
gaurav23617 <gaurav23617@gmail.com>
2025-06-19 22:31:20 +0530

    home manager modules added

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -3,4 +3,38 @@
 {
   # List your module files here
   # my-module = import ./my-module.nix;
+  imports = [
+    # fonts
+    ./fonts.nix
+
+    # Browsers
+    ./browsers/zen.nix
+    ./browsers/browser.nix
+
+    # Cli tools
+    ./cli/nh.nix
+    ./cli/direnv.nix
+    ./cli/git.nix
+    ./cli/gh.nix
+
+    # Editor and IDEs
+    ./editor/neovim.nix
+    ./editor/vscode.nix
+
+    # Miscellaneous
+    ./misc/cpufreq.nix
+    ./misc/lact.nix
+    ./misc/nix-ld.nix
+    ./misc/thunar.nix
+    ./misc/tlp.nix
+
+    # Shells
+    ./shell/zsh.nix
+    ./shell/nushell.nix
+
+    # Terminal
+    ./terminal/ghostty.nix
+    ./terminal/kitty.nix
+
+  ];
 }
