1c3fb8ad8be1baf445a78c8ef619f80c144571a0
gaurav23617 <gaurav23617@gmail.com>
2025-06-19 21:52:48 +0530

    fonts config eanble added

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -1,47 +1,57 @@
-{ pkgs, ... }:
+{ config, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.home-manager.fonts;
+in
 {
-  fonts = {
-    packages = with pkgs; [
-      # icon fonts
-      material-symbols
+  options.home-manager.fonts.enable = mkEnableOption "Enable Fonts Management";
 
-      # Sans(Serif) fonts
-      libertinus
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-emoji
-      roboto
-      (google-fonts.override { fonts = [ "Inter" ]; })
+  config = mkIf cfg.enable {
+    fonts = {
+      packages = with pkgs; [
+        # icon fonts
+        material-symbols
 
-      # monospace fonts
-      jetbrains-mono
+        # Sans(Serif) fonts
+        libertinus
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-emoji
+        roboto
+        (google-fonts.override { fonts = [ "Inter" ]; })
 
-      # nerdfonts
-      nerd-fonts.jetbrains-mono
-      nerd-fonts.symbols-only
-      nerd-fonts.caskaydia-cove
-      nerd-fonts.bigblue-terminal
-      nerd-fonts.victor-mono
-      nerd-fonts.zed-mono
-      nerd-fonts.mononoki
-      nerd-fonts.heavy-data
-      nerd-fonts.inconsolata
-      nerd-fonts.fira-code
-      nerd-fonts.symbols-only
-      maple-mono.truetype
-      maple-mono.NF-unhinted
+        # monospace fonts
+        jetbrains-mono
 
-    ];
+        # nerdfonts
+        nerd-fonts.jetbrains-mono
+        nerd-fonts.symbols-only
+        nerd-fonts.caskaydia-cove
+        nerd-fonts.bigblue-terminal
+        nerd-fonts.victor-mono
+        nerd-fonts.zed-mono
+        nerd-fonts.mononoki
+        nerd-fonts.heavy-data
+        nerd-fonts.inconsolata
+        nerd-fonts.fira-code
+        nerd-fonts.symbols-only
+        maple-mono.truetype
+        maple-mono.NF-unhinted
 
-    # causes more issues than it solves
-    enableDefaultPackages = false;
+      ];
 
-    # user defined fonts
-    fontconfig.defaultFonts = {
-      serif = [ "Libertinus Serif" ];
-      sansSerif = [ "Inter" ];
-      monospace = [ "JetBrains Mono Nerd Font" ];
-      emoji = [ "Noto Color Emoji" ];
+      # causes more issues than it solves
+      enableDefaultPackages = false;
+
+      # user defined fonts
+      fontconfig.defaultFonts = {
+        serif = [ "Libertinus Serif" ];
+        sansSerif = [ "Inter" ];
+        monospace = [ "JetBrains Mono Nerd Font" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
     };
   };
 }
