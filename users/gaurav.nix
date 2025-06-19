0977647cce2342d60a371812a6fe6e3d818c50be
gaurav23617 <gaurav23617@gmail.com>
2025-06-19 22:50:23 +0530

    hom session variables added

--- users/gaurav.nix
+++ users/gaurav.nix
@@ -34,4 +34,10 @@
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "25.05";
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    BROWSER = "zen";
+    TERMINAL = "ghostty";
+  };
+
 }
