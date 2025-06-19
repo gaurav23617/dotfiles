d7198c959c5fb2561a5b61eb11ec49092c887b5b
gaurav23617 <gaurav23617@gmail.com>
2025-06-19 22:12:21 +0530

    tlp added

---
+++ modules/home-manager/misc/tlp.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.misc.tlp;
+in
+{
+  options.misc.tlp.enable = mkEnableOption "Enable TLP (Linux Advanced Power Management)";
+  config = mkIf cfg.enable {
+    services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave"; # schedutil powersave, ondemand
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power"; # power, balance_power
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 50;
+
+        # Protect battery
+        START_CHARGE_THRESH_BAT0 = 82;
+        STOP_CHARGE_THRESH_BAT0 = 95;
+        START_CHARGE_THRESH_BAT1 = 82;
+        STOP_CHARGE_THRESH_BAT1 = 95;
+      };
+    };
+  };
+}
