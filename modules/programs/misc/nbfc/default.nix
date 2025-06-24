3673cb773df0148bdbfc12d89ba4e58b5c983a48
gaurav23617 <gaurav23617@gmail.com>
2025-06-24 20:53:58 +0530

    nbfc

--- modules/programs/misc/nbfc/default.nix
+++ modules/programs/misc/nbfc/default.nix
@@ -5,20 +5,37 @@
   username,
   ...
 }:
-let
-  command = "bin/nbfc_service --config-file '/home/${username}/.config/nbfc.json'";
 
+let
+  # Override nbfc-linux to fix missing libcurl and non-executable nbfc-qt
+  patchedNbfc = inputs.nbfc-linux.packages.x86_64-linux.default.overrideAttrs (old: {
+    buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.curl ];
+    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.pkg-config ];
+
+    # Remove nbfc-qt if it exists but is not executable
+    postFixup = ''
+      if [ -e "$out/bin/nbfc-qt" ] && [ ! -x "$out/bin/nbfc-qt" ]; then
+        echo "Removing non-executable nbfc-qt..."
+        rm -f "$out/bin/nbfc-qt"
+      fi
+    '';
+  });
+
+  command = "bin/nbfc_service --config-file '/home/${username}/.config/nbfc.json'";
 in
 {
-  environment.systemPackages = with pkgs; [
-    inputs.nbfc-linux.packages.x86_64-linux.default
+  environment.systemPackages = [
+    patchedNbfc
   ];
+
   systemd.services.nbfc_service = {
     enable = true;
     description = "NoteBook FanControl service";
-    serviceConfig.Type = "simple";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${patchedNbfc}/${command}";
+    };
     path = [ pkgs.kmod ];
-    script = "${inputs.nbfc-linux.packages.x86_64-linux.default}/${command}";
     wantedBy = [ "multi-user.target" ];
   };
 }
