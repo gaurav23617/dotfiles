13ca66680eb932a979d225f87cc74be8487f608a
gaurav23617 <gaurav23617@gmail.com>
2025-07-08 07:25:32 +0530

    xdg mime removed

--- modules/programs/browser/brave/default.nix
+++ modules/programs/browser/brave/default.nix
@@ -6,16 +6,6 @@
         home.packages = [
           pkgs.brave
         ];
-
-        xdg.mimeApps = {
-          enable = true;
-          # defaultApplications = {
-          #   "text/html" = "brave-browser.desktop";
-          #   "x-scheme-handler/http" = "brave-browser.desktop";
-          #   "x-scheme-handler/https" = "brave-browser.desktop";
-          # };
-        };
-
         # Optional: Add a desktop shortcut
         programs.bash = {
           enable = true;
