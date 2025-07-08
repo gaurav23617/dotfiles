8d6151904c7ee3225c223eb33d0b2bf04a8703da
gaurav23617 <gaurav23617@gmail.com>
2025-07-08 22:08:17 +0530

    git added

---
+++ modules/programs/cli/git.nix
@@ -0,0 +1,22 @@
+{ ... }:
+
+{
+  home-manager.sharedModules = [
+    (
+      { config, pkgs, ... }:
+      {
+        programs.git = {
+          enable = true;
+          lfs.enable = true;
+        };
+
+        home.packages = with pkgs; [
+          diff-so-fancy
+        ];
+
+        home.file.".gitconfig".source =
+          config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/git/.gitconfig";
+      }
+    )
+  ];
+}
