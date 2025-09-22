81c90ef50f9443f53526ab709ca3186f03682b0a
gaurav23617 <gaurav23617@gmail.com>
2025-09-22 23:17:43 +0530

    feat: nix settings substituters uncommented
    Signed-off-by: gaurav23617 <gaurav23617@gmail.com>

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -6,7 +6,6 @@
     allowBroken = true;
   };
   nix = {
-    nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
     channel.enable = false;
     extraOptions = ''
       warn-dirty = false
@@ -15,19 +14,17 @@
       download-buffer-size = 262144000; # 250 MB (250 * 1024 * 1024)
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
-      # substituters = [
-      #   # high priority since it's almost always used
-      #   "https://cache.nixos.org?priority=10"
-      #
-      #   "https://hyprland.cachix.org"
-      #   "https://nix-community.cachix.org"
-      #   "https://numtide.cachix.org"
-      # ];
-      # trusted-public-keys = [
-      #   "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      #   "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      # ];
+      substituters = [
+        # high priority since it's almost always used
+        "https://cache.nixos.org?priority=10"
+
+        "https://nix-community.cachix.org"
+        "https://numtide.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      ];
     };
     gc = {
       automatic = true;
