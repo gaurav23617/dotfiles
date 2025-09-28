e060019441609a54426f5cfc01219f017bbf1a91
gaurav23617 <gaurav23617@gmail.com>
2025-09-28 05:52:58 +0530

    feat: security files added
    Signed-off-by: gaurav23617 <gaurav23617@gmail.com>

---
+++ modules/nixos/security.nix
@@ -0,0 +1,12 @@
+{ config, ... }: {
+  security = {
+    # allow wayland lockers to unlock the screen
+    pam.services.hyprlock.text = "auth include login";
+
+    # userland niceness
+    rtkit.enable = true;
+
+    # don't ask for password for wheel group
+    # sudo.wheelNeedsPassword = false;
+  };
+}
