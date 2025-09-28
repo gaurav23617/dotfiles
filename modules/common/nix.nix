e24453e783d380b8e9d8099e8c6a7123466a6162
gaurav23617 <gaurav23617@gmail.com>
2025-09-28 14:53:58 +0530

    feat: cashe added
    Signed-off-by: gaurav23617 <gaurav23617@gmail.com>

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -11,7 +11,7 @@
       warn-dirty = false
     '';
     settings = {
-      download-buffer-size = 262144000; # 250 MiB
+      download-buffer-size = 262144000; # 250 MB (250 * 1024 * 1024)
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
       substituters = [
@@ -25,6 +25,10 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       ];
+
+      extra-substituters = [ "https://vicinae.cachix.org" ];
+      extra-trusted-public-keys =
+        [ "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc=" ];
     };
     gc = {
       automatic = true;
