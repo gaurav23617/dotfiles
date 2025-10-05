e3fb15c06e16d02f609589098c224faddc7be56e
gaurav23617 <gaurav23617@gmail.com>
2025-10-05 23:06:37 +0530

    fix: homedirectory fixed
    Signed-off-by: gaurav23617 <gaurav23617@gmail.com>

--- hosts/darwin/coffee/secrets/default.nix
+++ hosts/darwin/coffee/secrets/default.nix
@@ -1,19 +1,17 @@
-{ config, pkgs, inputs, ... }: {
+{ config, pkgs, inputs, homeDirectory, ... }: {
   imports = [ inputs.sops-nix.homeManagerModules.sops ];
 
   sops = {
-    age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+    age.keyFile = "${homeDirectory}/.config/sops/age/keys.txt";
 
     defaultSopsFile = ../../../../secrets/secrets.yaml;
 
     secrets = {
-      sshconfig = { path = "${config.home.homeDirectory}/.ssh/config"; };
-      github-key = { path = "${config.home.homeDirectory}/.ssh/github"; };
-      signing-key = { path = "${config.home.homeDirectory}/.ssh/key"; };
-      signing-pub-key = { path = "${config.home.homeDirectory}/.ssh/key.pub"; };
-      allowed-signers = {
-        path = "${config.home.homeDirectory}/.ssh/allowed_signers";
-      };
+      sshconfig = { path = "${homeDirectory}/.ssh/config"; };
+      github-key = { path = "${homeDirectory}/.ssh/github"; };
+      signing-key = { path = "${homeDirectory}/.ssh/key"; };
+      signing-pub-key = { path = "${homeDirectory}/.ssh/key.pub"; };
+      allowed-signers = { path = "${homeDirectory}/.ssh/allowed_signers"; };
     };
   };
 
