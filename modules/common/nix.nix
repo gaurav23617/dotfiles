4be590d17c8d33706ea75dc75c55b81d8b4cc3b8
gaurav23617 <gaurav23617@gmail.com>
2025-10-15 08:34:17 +0530

    feat: some of nix config change for darwin

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,5 +1,6 @@
 # Nix configuration for NixOS
-{ config, inputs, ... }: {
+{ config, inputs, ... }:
+{
   # Allow unfree packages
   nixpkgs.config.allowBroken = true;
   nix = {
@@ -7,30 +8,41 @@
     extraOptions = ''
       warn-dirty = false
     '';
+    optimise.automatic = true;
     settings = {
       download-buffer-size = 262144000; # 250 MB (250 * 1024 * 1024)
-      auto-optimise-store = true;
-      experimental-features = [ "nix-command" "flakes" ];
+      # auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
       substituters = [
         # high priority since it's almost always used
         "https://cache.nixos.org?priority=10"
-
         "https://nix-community.cachix.org"
         "https://numtide.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       ];
-
       extra-substituters = [ "https://vicinae.cachix.org" ];
-      extra-trusted-public-keys =
-        [ "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc=" ];
+      extra-trusted-public-keys = [ "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc=" ];
+
+      # ADD THESE NEW LINES:
+      builders-use-substitutes = true; # Use substitutes when building
+      max-jobs = 2; # Don't build anything locally, only download binaries
+      fallback = true;
     };
     gc = {
       automatic = true;
-      persistent = true;
-      dates = "weekly";
+      # persistent = true;
+      interval = {
+        Weekday = 0;
+        Hour = 0;
+        Minute = 0;
+      };
       options = "--delete-older-than 7d";
     };
   };
