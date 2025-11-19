14582820c1e3fd58952ac64c01e453436d8341e8
gaurav23617 <gaurav23617@gmail.com>
2025-11-19 09:29:22 +0530

    feat: npm config removed and node config added

---
+++ home/node.nix
@@ -0,0 +1,56 @@
+{
+  pkgs-extra,
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  home = let
+    npmGlobalDir = "${config.home.homeDirectory}/.npm";
+  in {
+    packages = with pkgs; [
+      npm-check-updates # (ncu) Find newer versions of package dependencies and check outdated npm packages locally or globally.
+      nodePackages.pnpm
+      bun
+      pkgs-extra.npkill # remove node_modules from child directories
+      nest-cli
+      nodejs_24
+      husky
+      biome
+    ];
+
+    sessionVariables = {
+      # https://github.com/npm/cli/issues/7857#issuecomment-2481331001
+      NODE_OPTIONS = "--disable-warning=ExperimentalWarning";
+    };
+
+    sessionPath = ["${npmGlobalDir}/bin"];
+
+    activation.init = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      mkdir -p ${npmGlobalDir}/bin \
+               ${npmGlobalDir}/lib
+    '';
+
+    file = {
+      ".npmrc".text = ''
+        prefix=${npmGlobalDir}
+        //registry.npmjs.org/:_authToken=''${NPM_TOKEN}
+      '';
+
+      ".bunfig.toml".text = ''
+        [runtime]
+        logLevel = "debug"
+        telemetry = false
+
+        [install]
+        optional = true
+        dev = true
+        peer = true
+        production = false
+        exact = true
+
+        auto = "fallback"
+      '';
+    };
+  };
+}
