6dc4fc256f64568867a36489b1eeec11857cef1c
gaurav2361 <gaurav23617@gmail.com>
2025-12-26 15:13:04 +0530

    chore: zsh vi node jj removed

--- config/zsh/Keybindings.zsh
+++ config/zsh/Keybindings.zsh
@@ -13,4 +13,3 @@ bindkey -s ^f "tmux-sessionizer\n"
 # bindkey -M vicmd 'k' history-substring-backward
 # bindkey -M vicmd 'j' history-substring-search-down
 ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
-ZVM_VI_INSERT_ESCAPE_BINDKEY=jj
