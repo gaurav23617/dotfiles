2dfdd73b33fe529665200fc07d519e2fd971d192
gaurav2361 <gaurav23617@gmail.com>
2026-01-09 10:05:08 +0530

    chore: flake update

--- flake.nix
+++ flake.nix
@@ -122,14 +122,14 @@
             nixpkgs.overlays = [
               inputs.brew-nix.overlays.default
               # Override Spotify with correct hash for macOS
-              (final: prev: {
-                spotify = prev.spotify.overrideAttrs (oldAttrs: {
-                  src = prev.fetchurl {
-                    url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
-                    sha256 = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
-                  };
-                });
-              })
+              # (final: prev: {
+              #   spotify = prev.spotify.overrideAttrs (oldAttrs: {
+              #     src = prev.fetchurl {
+              #       url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
+              #       sha256 = "sha256-gEZxRBT7Jo2m6pirf+CreJiMeE2mhIkpe9Mv5t0RI58=";
+              #     };
+              #   });
+              # })
             ];
           }
           home-manager.darwinModules.home-manager
